b8202d86f6cb888e3bc3e09a9bf9f3b1bc4c8b63
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-24 18:51:54 -0700

    Add PUT action

--- CretaceousApi/Program.cs
+++ CretaceousApi/Program.cs
@@ -8,8 +8,7 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<CretaceousApiContext>(
                   dbContextOptions => dbContextOptions
                     .UseMySql(
-                      builder.Configuration["ConnectionStrings:DefaultConnection"], 
-                      ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                      builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
                     )
                   )
                 );
@@ -17,25 +16,16 @@ builder.Services.AddDbContext<CretaceousApiContext>(
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-else
+else 
 {
-    app.UseHttpsRedirection();
+  app.UseHttpsRedirection();
 }
 
 app.UseAuthorization();
